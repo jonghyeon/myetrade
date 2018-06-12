f5c9e639e3d19dcab8f15320387db56c8668820f
Owen Kwon <pinebud77@hotmail.com>
2018-06-12 12:26:35 -0700

    restructure json load/store

--- README.md
+++ README.md
@@ -12,30 +12,3 @@ contents of configuration file : config.py
 Target Implementation :
 * two or three simple trading algorithms
 * maintain ticker list through JSON or XML
-
-JSON format : <br/>
-{\
-  "accounts": [\
-    {\
-      "cash_to_trade": 1000000055.98,\
-      "id": "account_number",\
-      "net_value": 100000000000000,\
-      "stocks": [\
-        {\
-          "count": 260000000000,\
-          "last_sell_price": 0.0,\
-          "net_purchase_share": 0.5,\
-          "symbol": "AAPL",\
-          "value": 4971.98\
-        },\
-        {\
-          "count": 0,\
-          "last_sell_price": 0.0,\
-          "net_purchase_share": 0.5,\
-          "symbol": "MSFT",\
-          "value": 101.5\
-        }\
-      ]\
-    }\
-]\
-}\
--- main.py
+++ main.py
@@ -11,6 +11,40 @@ import algorithms
 json_config_file = 'config.json'
 
 
+def load_json_account(json_account, account):
+    account.mode = json_account['mode']
+
+
+def store_json_account(json_account, account):
+    json_account['mode'] = account.mode
+    json_account['net_value'] = account.net_value
+    json_account['cash_to_trade'] = account.cash_to_trade
+
+
+def load_json_stock(json_stock, stock):
+    stock.last_sell_price = json_stock['last_sell_price']
+    stock.last_buy_price = json_stock['last_buy_price']
+    stock.last_value = json_stock['last_value']
+    stock.last_count = json_stock['last_count']
+    stock.budget = stock.account.net_value * json_stock['trade_share']
+    stock.algorithm_string = json_stock['algorithm']
+    if json_stock['stance'] == 'moderate':
+        stock.stance = algorithms.MODERATE
+    elif json_stock['stance'] == 'aggressive':
+        stock.stance = algorithms.AGGRESSIVE
+    else:
+        stock.stance = algorithms.CONSERVATIVE
+
+
+def store_json_stock(json_stock, stock):
+    json_stock['last_sell_price'] = stock.last_sell_price
+    json_stock['last_buy_price'] = stock.last_buy_price
+    json_stock['last_value'] = stock.last_value
+    json_stock['last_count'] = stock.last_count
+    json_stock['count'] = stock.count
+    json_stock['value'] = stock.value
+
+
 if __name__ == '__main__':
     #logging.basicConfig(level=logging.DEBUG)
     logging.basicConfig(level=logging.INFO)
@@ -38,37 +72,18 @@ if __name__ == '__main__':
 
     for json_account in config['accounts']:
         account = client.get_account(json_account['id'])
-        account.mode = json_account['mode']
-
-        json_account['net_value'] = account.net_value
-        json_account['cash_to_trade'] = account.cash_to_trade
+        load_json_account(json_account, account)
 
         for json_stock in json_account['stocks']:
-            json_stock['last_value'] = json_stock['value']
             stock = account.get_stock(json_stock['symbol'])
-            if stock:
-                json_stock['value'] = stock.value
-                json_stock['count'] = stock.count
-            else:
+            if not stock:
                 quote = client.get_quote(json_stock['symbol'])
-                json_stock['value'] = quote.ask
-                json_stock['count'] = 0
                 stock = Stock(quote.symbol, account, account.session)
                 stock.value = quote.ask
                 stock.count = 0
                 account.add_empty_stock(stock)
-            stock.last_value = json_stock['last_value']
-            stock.last_buy_price = json_stock['last_buy_price']
-            stock.last_sell_price = json_stock['last_sell_price']
-            stock.last_count = json_stock['last_count']
-            stock.budget = account.net_value * json_stock['trade_share']
-            stock.algorithm_string = json_stock['algorithm']
-            if json_stock['stance'] == 'moderate':
-                stock.stance = algorithms.MODERATE
-            elif json_stock['stance'] == 'aggressive':
-                stock.stance = algorithms.AGGRESSIVE
-            else:
-                stock.stance = algorithms.CONSERVATIVE
+
+            load_json_stock(json_stock, stock)
 
             #execute main algorithms
             decision = 0
@@ -80,24 +95,25 @@ if __name__ == '__main__':
 
             #execute decision
             if decision != 0:
-                json_stock['last_count'] = stock.count
+                stock.last_count = stock.count
 
             if decision > 0:
-                stock.order(decision, order_id)
-                json_stock['last_buy_price'] = stock.value
+                stock.market_order(decision, order_id)
             elif decision < 0:
-                stock.order(decision, order_id)
-                json_stock['last_sell_price'] = stock.value
+                stock.market_order(decision, order_id)
 
             if decision != 0:
                 order_id += 1
-                json_stock['count'] = stock.count
-                if json_account['mode'] == 'setup':
-                    json_stock['last_buy_price'] = stock.value
-                    json_stock['last_sell_price'] = stock.value
+                if account.mode == 'setup':
+                    stock.last_buy_price = stock.value
+                    stock.last_sell_price = stock.value
+
+            store_json_stock(json_stock, stock)
 
         if account.mode == 'setup':
-            json_account['mode'] = 'run'
+            account.mode = 'run'
+
+        store_json_account(json_account, account)
 
     config['order_id'] = order_id
 
